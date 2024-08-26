da2d8327f490f69ffe94a3f923c831cfed868e62
karenlin56 <karen.lin.xyz@gmail.com>
2024-08-26 00:34:52 -0700

    final data_processing notebook and flask

--- show_data_from_database.py
+++ show_data_from_database.py
@@ -1,11 +1,13 @@
+# Import dependencies
 import numpy as np
 import psycopg2
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-
 from flask import Flask, jsonify
+
+
 
 # Connection between sqlalchemy and postgresql
 engine = create_engine('postgresql+psycopg2://postgres:postgres@localhost:5432/maternal_mortality')
@@ -41,9 +43,8 @@ app = Flask(__name__)
 
 
 
-### Displays the data on Black maternal mortality extracted from the SQL database
 
-### 'Homepage': Shows the possible routes
+# 'Homepage': Shows the possible routes
 @app.route("/")
 def welcome():
     return (
@@ -59,8 +60,11 @@ def welcome():
     )
 
 
+
+# Displays all the dates of the data and their corresponding indices
 @app.route("/api/v1.0/dates")
 def dates():
+
     date_data = session.query(Dates.date_m).all()
     session.close()
 
@@ -69,13 +73,16 @@ def dates():
         result_dict = {}
         result_dict["Date"] = result[0]
         dates_dict.append(result_dict)
+
     return jsonify(dates_dict)
 
 
 
 
+# Displays the data on Black maternal mortality extracted from the SQL database
 @app.route("/api/v1.0/black")
 def blackData():
+
     black_data = session.query(Dates.date_m, Black.maternal_deaths, Black.live_births, Black.maternal_mortality_rate).where(Dates.date_index == Black.date_index).order_by(Dates.date_m).all()
     session.close()
 
@@ -87,19 +94,20 @@ def blackData():
         result_dict["Live Births"] = result[2]
         result_dict["Maternal Mortality Rate"] = result[3]
         black_dict.append(result_dict)
+        
     return jsonify(black_dict)
 
 
 
 
-
-### Displays the data on Hispanic maternal mortality extracted from the SQL database
+# Displays the data on Hispanic maternal mortality extracted from the SQL database
 @app.route("/api/v1.0/hispanic")
 def hispanicData():
+
     hispanic_data = session.query(Dates.date_m, Hispanic.maternal_deaths, Hispanic.live_births, Hispanic.maternal_mortality_rate).where(Dates.date_index == Hispanic.date_index).order_by(Dates.date_m).all()
     session.close()
 
-
+    # List of dictionaries with each dictionary corresponding to a row of data
     hispanic_dict = []
     for result in hispanic_data:
         result_dict = {}
@@ -108,17 +116,20 @@ def hispanicData():
         result_dict["Live Births"] = result[2]
         result_dict["Maternal Mortality Rate"] = result[3]
         hispanic_dict.append(result_dict)
+
     return jsonify(hispanic_dict)
 
 
 
 
-### Displays the data on Native-Indian (Native-American) maternal mortality extracted from the SQL database
+# Displays the data on Native-Indian (Native-American) maternal mortality extracted from the SQL database
 @app.route("/api/v1.0/native-indian")
 def native_indian_Data():
+
     native_data = session.query(Dates.date_m, Native_Indian.maternal_deaths, Native_Indian.live_births, Native_Indian.maternal_mortality_rate).where(Dates.date_index == Native_Indian.date_index).order_by(Dates.date_m).all()
     session.close()
 
+    # List of dictionaries with each dictionary corresponding to a row of data
     native_dict = []
     for result in native_data:
         result_dict = {}
@@ -127,17 +138,20 @@ def native_indian_Data():
         result_dict["Live Births"] = result[2]
         result_dict["Maternal Mortality Rate"] = result[3]
         native_dict.append(result_dict)
+
     return jsonify(native_dict)
 
 
 
 
-### Displays the data on Asian maternal mortality extracted from the SQL database
+# Displays the data on Asian maternal mortality extracted from the SQL database
 @app.route("/api/v1.0/asian")
 def asianData():
+
     asian_data = session.query(Dates.date_m, Asian.maternal_deaths, Asian.live_births, Asian.maternal_mortality_rate).where(Dates.date_index == Asian.date_index).order_by(Dates.date_m).all()
     session.close()
 
+    # List of dictionaries with each dictionary corresponding to a row of data
     asian_dict = []
     for result in asian_data:
         result_dict = {}
@@ -146,15 +160,17 @@ def asianData():
         result_dict["Live Births"] = result[2]
         result_dict["Maternal Mortality Rate"] = result[3]
         asian_dict.append(result_dict)
+
     return jsonify(asian_dict)
 
 
 
 
 
-### Displays the data on Pacific-Islander maternal mortality extracted from the SQL database
+# Displays the data on Pacific-Islander maternal mortality extracted from the SQL database
 @app.route("/api/v1.0/pacific-islander")
 def pacificData():
+
     pacific_data = session.query(Dates.date_m, Pacific_Islander.maternal_deaths, Pacific_Islander.live_births, Pacific_Islander.maternal_mortality_rate).where(Dates.date_index == Pacific_Islander.date_index).order_by(Dates.date_m).all() 
     session.close()
 
@@ -166,13 +182,15 @@ def pacificData():
         result_dict["Live Births"] = result[2]
         result_dict["Maternal Mortality Rate"] = result[3]
         pacific_dict.append(result_dict)
+
     return jsonify(pacific_dict)
 
 
 
-### Displays the data on White maternal mortality extracted from the SQL database
+# Displays the data on White maternal mortality extracted from the SQL database
 @app.route("/api/v1.0/white")
 def whiteData():
+
     white_data = session.query(Dates.date_m, White.maternal_deaths, White.live_births, White.maternal_mortality_rate).where(Dates.date_index == White.date_index).order_by(Dates.date_m).all()
     session.close()
 
@@ -184,10 +202,16 @@ def whiteData():
         result_dict["Live Births"] = result[2]
         result_dict["Maternal Mortality Rate"] = result[3]
         white_dict.append(result_dict)
+
     return jsonify(white_dict)
 
+
+
+
+# Displays the mean maternal mortality rates of each racial group
 @app.route("/api/v1.0/aggregate")
 def aggregate():
+
     black_avg = session.query(func.avg(Black.maternal_mortality_rate)).first()
     hispanic_avg = session.query(func.avg(Hispanic.maternal_mortality_rate)).first()
     native_american_avg = session.query(func.avg(Native_Indian.maternal_mortality_rate)).first()
@@ -204,6 +228,7 @@ def aggregate():
     aggregate_dict["Average Asian Maternal Mortality Rate"] = asian_avg[0]
     aggregate_dict["Average Pacific Islander Maternal Mortality Rate"] = pacific_avg[0]
     aggregate_dict["Average White Maternal Mortality Rate"] = white_avg[0]
+
     return jsonify(aggregate_dict)
 
 
